894738a2ff6d77bfbaf55cf1a9285ccf03e4fc82
shilpa85 <s.dhapola85@gmail.com>
2020-07-16 14:57:43 +0530

    Issue fix

---
+++ src/src/components/products/Index.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import { getAllProducts} from "../../actions/products";
+import { useSelector, useDispatch } from "react-redux";
+import ProductItem from './ProductItem.js';
+import Filter from './Filter.js';
+import './product.scss';
+
+function Products (props) {
+
+    const { productList } = useSelector(state => state.products);
+
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+         dispatch(getAllProducts());
+    }, []);
+
+
+
+    return(	
+        <>
+            <Filter  />		
+            <div className="product-container">
+                {productList.length > 0 ?
+                productList.map(product => 
+                    <ProductItem key={product.id} product={product} />
+                )
+                :  <div className="no-results" >No results found.</div>
+            }
+            </div>
+        </>
+    )
+}
+
+export default Products;
---
+++ src/src/utils/api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const baseURL = 'https://xebiascart.herokuapp.com/'
+
+export default {
+
+ auth: {
+    login: data => {
+       return axios.get(`${baseURL}users?username=${data.username}`);
+      }
+  },
+
+  products: {
+    fiterBy : data => {
+      let URL;
+      if( data !== ''){
+        URL = `${baseURL}products?${data}`;
+      } else {
+        URL = `${baseURL}products`;
+      } 
+
+      return axios({
+        method: "GET",
+        url: URL,
+      })
+    },
+
+    fetchAll : data => {
+      return axios({
+        method: "GET",
+        url: `${baseURL}products`,
+      })
+    },
+
+  }
+
+};
